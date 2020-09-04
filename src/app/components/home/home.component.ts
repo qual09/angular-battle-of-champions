47b94163575aa782ec6f0000862470fdb06dab61
Hieu Chu Pham Xuan <hieu.chu@lingarogroup.com>
2020-09-04 14:36:22 +0200

    champion select

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Champion } from '../../models/champion';
+import { CHAMPIONS } from '../../constants/champions';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  championsList: Champion[] = CHAMPIONS;
+  champions: Champion[];
 
-  constructor() { }
+  constructor() {
+    this.champions = [
+      { name: '', class: '' },
+      { name: '', class: '' },
+      { name: '', class: '' },
+      { name: '', class: '' },
+      { name: '', class: '' },
+    ]
+  }
 
   ngOnInit(): void {
+
+  }
+
+  setClass(championName: string) {
+    this.champions.forEach(champion => {
+      if (champion.name === championName) {
+        this.championsList.forEach(championTemplate => {
+          if (championTemplate.name === championName) {
+            champion.class = championTemplate.class;
+          }
+        });
+      }
+    });
+  }
+
+  log() {
+    console.log(this.champions);
   }
 
 }
---
+++ src/app/constants/champions.ts
@@ -0,0 +1,24 @@
+import { Champion } from '../models/champion';
+
+export const CHAMPIONS: Champion[] = [
+  {
+    name: 'Morias',
+    class: 'Warrior',
+  },
+  {
+    name: 'Angela',
+    class: 'Mage',
+  },
+  {
+    name: 'Eliza',
+    class: 'Assassin',
+  },
+  {
+    name: 'Strife',
+    class: 'Hunter',
+  },
+  {
+    name: 'Kame',
+    class: 'Monk',
+  },
+]
