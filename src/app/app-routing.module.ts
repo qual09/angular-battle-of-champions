f668f41fdf5e90d391d311cbffd87c3ec22ba1ea
HCPX <cpx.hieu@gmail.com>
2020-09-04 18:56:38 +0200

    removed routing for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
