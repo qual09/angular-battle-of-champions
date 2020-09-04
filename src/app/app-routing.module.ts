1eec3fb389128f3a9f04ed4e878563d4b3f6daac
HCPX <cpx.hieu@gmail.com>
2020-09-05 00:16:20 +0200

    todos added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+// import { HowComponent } from './components/how/how.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
   // { path: 'home', component: HomeComponent },
+  // { path: 'how', component: HowComponent },
 ];
 
 @NgModule({
