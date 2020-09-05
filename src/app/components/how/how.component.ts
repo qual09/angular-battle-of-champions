8dd69f4121b01ac3ce7ec5cd652730988c690af6
HCPX <cpx.hieu@gmail.com>
2020-09-05 13:22:36 +0200

    dynamic how to class number

--- src/app/components/how/how.component.ts
+++ src/app/components/how/how.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Champion } from '../../models/champion';
+import { CHAMPIONS } from '../../constants/champions';
+
 @Component({
   selector: 'app-how',
   templateUrl: './how.component.html',
   styleUrls: ['./how.component.css']
 })
 export class HowComponent implements OnInit {
+  championsList: Champion[] = CHAMPIONS;
 
   constructor() { }
 
